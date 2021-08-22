047f7d908495a376aa398e337642de331199b40f
Frans van Ek <frans@fransvanek.nl>
2021-08-22 19:17:13 +0200

    merge

--- JLio.Core/Models/JLioFunctionRegistration.cs
+++ JLio.Core/Models/JLioFunctionRegistration.cs
@@ -11,7 +11,5 @@ namespace JLio.Core.Models
 
         public Type Type { get; }
 
-        //todo implement the ctor compiled lamba expression for performance instead of using the reflection activator.creteinstance method.
-        // see: https://vagifabilov.wordpress.com/2010/04/02/dont-use-activator-createinstance-or-constructorinfo-invoke-use-compiled-lambda-expressions/
     }
 }
