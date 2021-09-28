90aa376dfaac6690019faa2961b5f6e01b74e309
Frans van Ek <frans@fransvanek.nl>
2021-09-28 10:51:17 +0200

    implementation of the toString function (#21)
    * implementation of the toString function
    
    * moved a ;

--- JLio.CLient/ParseOptions.cs
+++ JLio.CLient/ParseOptions.cs
@@ -29,7 +29,7 @@ namespace JLio.Client
                 .Register<Datetime>()
                 .Register<NewGuid>()
                 .Register<Concat>()
-                ;
+                .Register<ToString>();
 
             return new ParseOptions
             {
