a4f66ff84f7196c0ef030c1117ff6d4b730edf42
Frans van Ek <frans@fransvanek.nl>
2021-10-06 09:57:10 +0200

    fixed two test on the new type for function execution result

--- JLio.Core/Models/FunctionSupportedValue.cs
+++ JLio.Core/Models/FunctionSupportedValue.cs
@@ -16,10 +16,13 @@ namespace JLio.Core.Models
         {
             var result = Function.Execute(currentToken, dataContext, context);
             if (!result.Success)
+            {
                 context.LogError(CoreConstants.FunctionExecution,
                     $"Execute of function {Function.FunctionName} failed");
+                return JLioFunctionResult.Failed(result.Data);
+            }
 
-            return result.Data;
+            return JLioFunctionResult.SuccessFul(result.Data);
         }
 
         public string GetStringRepresentation()
